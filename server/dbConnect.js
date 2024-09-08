e161cfb59f8389db845e1d46c1596792270641bb
Luka Koll <lukakoll@lev-05254.apn.wlan.private.upenn.edu>
2024-09-07 22:36:24 -0400

    Initial commit

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,11 +1,9 @@
 const mysql = require('mysql');
+require("dotenv").config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const dbUrl= 'mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}'
+
+const db = mysql.createConnection();
 
 db.connect((err) => {
     if (err) {
